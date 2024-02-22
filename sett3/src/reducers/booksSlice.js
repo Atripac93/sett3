90f9ac05e736d614be65dc9f9685579e178ae376
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-22 11:08:31 +0100

    Update booksSlice.js

--- sett3/src/reducers/booksSlice.js
+++ sett3/src/reducers/booksSlice.js
@@ -1,35 +1,46 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios"
+import axios from "axios";
 
+const initialState = {
+  books: [],
+  isLoading: false,
+  error: null,
+};
 
-const initialState ={
-    book:[],
-    isLoading: false,
-    error: null,
-}
-export const getBook =createAsyncThunk(
-`book/GETbook`,
-async()=>{
-    try{
-        const resp = await axios get("https://epibooks.onrender.com/")
-        return await resp.data
-    }catch(e){
-        console.error(e.message)
-    }
-}
-)
-    
+export const getBooks = createAsyncThunk("books/GETbooks", async () => {
+  try {
+    const resp = await axios.get("https://epibooks.onrender.com/");
+    return resp.data;
+  } catch (e) {
+    console.error(e.message);
+    throw e; // Rilancia l'errore per gestirlo nel rejected
+  }
+});
+
 const booksSlice = createSlice({
-    name:"books",
-    initialState,
-    extraReducers(builder) => {
-        builder
-        .addCase(getBooks.pending,(state)=>{
-state.isLoading= true,
-state.error= null,
-        }
-            )
+  name: "books",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.books = action.payload;
+        state.error = null;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
 
+export const allBooks = (state) => state.booksData.books;
+export const isAllBooksLoading = (state) => state.booksData.isLoading;
+export const isAllBooksError = (state) => state.booksData.error;
 
-    }
-})
+export default booksSlice.reducer;
