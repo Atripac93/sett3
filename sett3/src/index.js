98110426636e0966c3eb0943357563f5f8de0890
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-22 11:08:34 +0100

    Update index.js

--- sett3/src/index.js
+++ sett3/src/index.js
@@ -4,27 +4,23 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import {Provider} from "react-redux",
-import {combineReducers, configureStore} from "@reduxjs/toolkit"
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
+import booksReduder from "../src/reducers/booksSlice";
+const reducer = combineReducers({
+  booksData: booksReduder,
+});
 
-const reducer =combineReducers({
-
-  const store = configureStore({
-
-  })
-})
-
-
-
+const store = configureStore({
+  reducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
-
+      <App />
     </Provider>
-
   </React.StrictMode>
 );
