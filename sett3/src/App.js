a83742837a733261c33eb487f576cf233ea68541
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-22 09:04:53 +0100

    Create App.js

---
+++ sett3/src/App.js
@@ -0,0 +1,13 @@
+import Navigation from "./Nav/Navigation";
+import AllBooks from "./Main/AllBooks";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <AllBooks />
+    </>
+  );
+};
+
+export default App;
